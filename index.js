b236285e4a7ce90fdbfa61bf8c68b537d8a14bb3
Maria Chapman <maria.chapman@thetrainline.com>
2019-08-14 13:19:26 +0100

    Now with words

--- index.js
+++ index.js
@@ -1,23 +1,51 @@
-module.exports = function lookAndSay(input) {
-  const inputLength = input.length;
-  let output = '';
-  let lastDigitSeenCount = 1;
+const { toWords } = require('number-to-words');
 
-  for (let i = 0; i < inputLength; i++) {
-    const currentDigit = input[i];
-    const previousDigit = i === 0 ? null : input[i - 1];
+module.exports = {
+  lookAndSay: function lookAndSay(input) {
+    const inputLength = input.length;
+    let output = '';
+    let count = 1;
 
-    if (currentDigit === previousDigit) {
-      lastDigitSeenCount++;
-    } else if (previousDigit !== null) {
-      output += `${lastDigitSeenCount}${previousDigit}`;
-      lastDigitSeenCount = 1;
+    for (let i = 0; i < inputLength; i++) {
+      const currentDigit = input[i];
+      const previousDigit = i === 0 ? null : input[i - 1];
+
+      if (currentDigit === previousDigit) {
+        count++;
+      } else if (previousDigit !== null) {
+        output += `${count}${previousDigit}`;
+        count = 1;
+      }
+
+      if (i === inputLength - 1) {
+        output += `${count}${currentDigit}`
+      }
     }
 
-    if (i === inputLength - 1) {
-      output += `${lastDigitSeenCount}${currentDigit}`
+    return output;
+  },
+
+  lookAndSayInWords: function lookAndSayInWords(input) {
+    const inputLength = input.length;
+    let output = '';
+    let count = 1;
+
+    for (let i = 0; i < inputLength; i++) {
+      const currentDigit = input[i];
+      const previousDigit = i === 0 ? null : input[i - 1];
+
+      if (currentDigit === previousDigit) {
+        count++;
+      } else if (previousDigit !== null) {
+        output += `${toWords(count)} ${previousDigit}${count > 1 ? 's' : ''}, `;
+        count = 1;
+      }
+
+      if (i === inputLength - 1) {
+        output += `${toWords(count)} ${currentDigit}${count > 1 ? 's' : ''}`
+      }
     }
+
+    return output;
   }
-
-  return output;
 }
--- index.test.js
+++ index.test.js
@@ -1,4 +1,4 @@
-const lookAndSay = require('.');
+const { lookAndSay, lookAndSayInWords } = require('.');
 
 describe('lookAndSay', () => {
   it('should exist', () => {
@@ -19,6 +19,29 @@ describe('lookAndSay', () => {
     'should return the correct output for %s',
     (input, expected) => {
       expect(lookAndSay(input)).toBe(expected)
+    }
+  )
+});
+
+describe('lookAndSayInWords', () => {
+  it('should exist', () => {
+    expect(lookAndSayInWords).toBeTruthy();
+  });
+
+  test.each([
+    ['1', 'one 1'],
+    ['11', 'two 1s'],
+    ['21', 'one 2, one 1'],
+    ['1211', 'one 1, one 2, two 1s'],
+    ['111221', 'three 1s, two 2s, one 1'],
+    ['312211', 'one 3, one 1, two 2s, two 1s'],
+    ['5', 'one 5'],
+    ['1111111111', 'ten 1s'],
+    // ['1213141516171819110', 'one 1, one 2, one 1, one 3, 1114111511161117111811192110']
+  ])(
+    'should return the correct output for %s',
+    (input, expected) => {
+      expect(lookAndSayInWords(input)).toBe(expected)
     }
   )
 });
